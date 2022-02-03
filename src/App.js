ce51c93f5eab5c1138b5014c344b3d6ae00b7be1
hinzzx <abvsuxhard@gmail.com>
2022-02-03 23:25:05 +0200

    added products component /soon to be fixed /

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import Navbar from "./components/Navbar/navbar";
 import Home from "./components/Home/Home";
 import "./components/Navbar/style.css";
 import Layout from "./components/Layout/Layout";
 import { Routes, Route } from "react-router-dom";
+import Products from "./components/products/products";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         {/* -------FAQ Page link-------- */}
         <Route path="/faq"></Route>
         {/* -------Product Page link-------- */}
-        <Route path="/products">
+        <Route path="/products" element={<Products/>}>
 
         </Route>
       </Routes>
