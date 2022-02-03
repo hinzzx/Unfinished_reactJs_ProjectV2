83f1870248cbb9c955cd315616e65138840745cf
hinzzx <abvsuxhard@gmail.com>
2022-02-04 00:49:27 +0200

    products page commented till next dev process

--- src/components/products/products.js
+++ src/components/products/products.js
@@ -8,44 +8,45 @@ import Fade from "react-reveal/Fade";
 
 const Products = () => {
   return (
-      <Fade bottom>
-    <div className="section-hero-inner">
-    <div className="centered">
-    <MDBContainer>
-      <MDBRow className="border-radzz">
-        <MDBCol size='md' className='col-example'>
-        <MDBCarousel showControls showIndicators>
-      <MDBCarouselInner>
-        <MDBCarouselItem className='active'>
-          <MDBCarouselElement src='https://mdbootstrap.com/img/new/slides/041.webp' alt='...' />
-          <MDBCarouselCaption>
-            <h5>First slide label</h5>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </MDBCarouselCaption>
-        </MDBCarouselItem>
-        <MDBCarouselItem>
-          <MDBCarouselElement src='https://mdbootstrap.com/img/new/slides/042.webp' alt='...' />
-          <MDBCarouselCaption>
-            <h5>Second slide label</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </MDBCarouselCaption>
-        </MDBCarouselItem>
-        <MDBCarouselItem>
-          <MDBCarouselElement src='https://mdbootstrap.com/img/new/slides/043.webp' alt='...' />
-          <MDBCarouselCaption>
-            <h5>Third slide label</h5>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </MDBCarouselCaption>
-        </MDBCarouselItem>
-      </MDBCarouselInner>
-    </MDBCarousel>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-    </div>
-  </div>
-  </Fade>
-);
-}
+    <></>
+  //     <Fade bottom>
+  //   <div className="section-hero-inner">
+  //   <div className="centered">
+  //   <MDBContainer>
+  //     <MDBRow className="border-radzz">
+  //       <MDBCol size='md' className='col-example'>
+  //       <MDBCarousel showControls showIndicators>
+  //     <MDBCarouselInner>
+  //       <MDBCarouselItem className='active'>
+  //         <MDBCarouselElement src='https://mdbootstrap.com/img/new/slides/041.webp' alt='...' />
+  //         <MDBCarouselCaption>
+  //           <h5>First slide label</h5>
+  //           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+  //         </MDBCarouselCaption>
+  //       </MDBCarouselItem>
+  //       <MDBCarouselItem>
+  //         <MDBCarouselElement src='https://mdbootstrap.com/img/new/slides/042.webp' alt='...' />
+  //         <MDBCarouselCaption>
+  //           <h5>Second slide label</h5>
+  //           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  //         </MDBCarouselCaption>
+  //       </MDBCarouselItem>
+  //       <MDBCarouselItem>
+  //         <MDBCarouselElement src='https://mdbootstrap.com/img/new/slides/043.webp' alt='...' />
+  //         <MDBCarouselCaption>
+  //           <h5>Third slide label</h5>
+  //           <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+  //         </MDBCarouselCaption>
+  //       </MDBCarouselItem>
+  //     </MDBCarouselInner>
+  //   </MDBCarousel>
+  //       </MDBCol>
+  //     </MDBRow>
+  //   </MDBContainer>
+  //   </div>
+  // </div>
+  // </Fade>
+ );
+ }
 
 export default Products;
