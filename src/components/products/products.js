814a846c1472af0c15f2eb08bb94a613183c94f7
hinzzx <abvsuxhard@gmail.com>
2022-02-14 18:35:51 +0200

    did few tests, more updates soon

--- src/components/products/products.js
+++ src/components/products/products.js
@@ -1,25 +1,39 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
-import './style.css';
+import "./style.css";
 
 const Products = () => {
   return (
     <Fade bottom>
       <div className="wrapper">
-      <div className="product-img">
-      <img src="https://i.pinimg.com/474x/cc/73/9e/cc739e01e297b11ed9e4ba5d8c9fc9bb.jpg" height="420" width="327" />
-    </div>
-    <div className="product-info">
-      <div className="product-text">
-        <h1>Harvest Vase</h1>
-        <h2>by studio and friends</h2>
-        <p>Harvest Vases are a reinterpretation<br /> of peeled fruits and vegetables as<br/> functional objects. The surfaces<br/> appear to be sliced and pulled aside,<br/> allowing room for growth. </p>
-      </div>
-      <div className="product-price-btn">
-        <p><span>78</span>$</p>
-        <button type="button">buy now</button>
-      </div>
-    </div>
+        <div className="product-img">
+          <img
+            src="https://i.pinimg.com/474x/cc/73/9e/cc739e01e297b11ed9e4ba5d8c9fc9bb.jpg"
+            height="420"
+            width="327"
+          />
+        </div>
+        <div className="product-info">
+          <div className="product-text">
+            <h1>Harvest Vase</h1>
+            <Fade left>
+              <h2>This card is soon to be responsive as well!</h2>
+            </Fade>
+            <p>
+              Harvest Vases are a reinterpretation
+              <br /> of peeled fruits and vegetables as
+              <br /> functional objects. The surfaces
+              <br /> appear to be sliced and pulled aside,
+              <br /> allowing room for growth.{" "}
+            </p>
+          </div>
+          <div className="product-price-btn">
+            <p>
+              <span>78</span>$
+            </p>
+            <button type="button">buy now</button>
+          </div>
+        </div>
       </div>
     </Fade>
   );
