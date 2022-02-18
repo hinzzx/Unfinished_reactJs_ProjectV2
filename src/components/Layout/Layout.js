1b1401d65c870166c22bdfcfd6f107e233bc088c
Stoyan <88034608+hinzzx@users.noreply.github.com>
2022-02-18 13:22:47 +0200

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,8 @@
 import Navbar from "../Navbar/navbar.js";
 import classes from "./Layout.css";
 
+// making sure the NavBar appears in every component in other words making it fixed.
+
 function Layout(props) {
   return (
     <div>
